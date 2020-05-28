422a15c6cd455e588105e0a0889df623d736da3d
Sam Seabourn <sam.seabourn@gmail.com>
2020-05-28 17:45:25 +1000

    Okay things are working. This thing is a beast

--- js/main.js
+++ js/main.js
@@ -1,9 +1,135 @@
+//Tile Array
+let allTiles = document.getElementsByClassName("pnt") // pnt stands for paintable. Trying to reduce the size of the HTML
+// Colors 
+var colorIdArray = []
+var color1 = { face1 : "", face2 : "", face3 : "" }
+var color2 = { face1 : "", face2 : "", face3 : "" }
+var selectedColor = null
+
+
+// buttons
+var changeColorOneBtn = document.getElementById("changeColorOne") 
+var changeColorTwoBtn = document.getElementById("changeColorTwo")
+var removeColorBrushBtn = document.getElementById("removeColorBrush")
+var selectColorOneBrushBtn = document.getElementById("selectColorOneBrush" )
+var selectColorTwoBrushBtn = document.getElementById("selectColorTwoBrush")
+
+// Event Listeners
+function initializeTileEvents(allTiles) {
+    for (let i = 0; i < allTiles.length; i++) {
+        allTiles[i].addEventListener("mouseover",( event ) => {
+            drawColors(selectedColor, event.target )
+        });
+    }
+}
+
+
+function initializeChangeColorOneButton() {
+    changeColorOneBtn.addEventListener("click", ( event ) => {
+        // var newColor = randomRGB()
+    })
+}
+function initializeChangeColorTwoButton() {
+    changeColorTwoBtn.addEventListener("click", ( event ) => {
+        console.log(event.target)
+    })
+}
+function initializeSelectColorOneBrushBtn() {
+    selectColorOneBrushBtn.addEventListener("click", ( event ) => {
+        event.target.setAttribute("stroke-width", "20px")
+        event.target.setAttribute("opacity", "1")
+
+        selectedColor = color1
+    })
+}
+
+initializeTileEvents(allTiles)
+initializeChangeColorOneButton()
+initializeChangeColorTwoButton()
+initializeSelectColorOneBrushBtn()
+
+
+// Color functions
+function changeColors(svgArray, color) {
+    var mediumColor = color
+    var darkColor = shiftColor(mediumColor, "shiftDown")
+    var lightColor = shiftColor(mediumColor, "shiftUp" )
+  
+    for (let i = 0; i < svgArray.length; i++) {
+      var color = svgArray[i]
+      if (color.classList.contains("f1")){
+        color.setAttribute('fill', lightColor)
+        color.setAttribute('stroke', lightColor)
+      }
+      else if (color.classList.contains("f3")){
+        color.setAttribute('fill', darkColor)
+        color.setAttribute('stroke', darkColor)
+      }
+      else {
+        color.setAttribute('fill', mediumColor)
+        color.setAttribute('stroke', mediumColor)
+      }
+    }
+  }
+  
+  function randomRGB() {
+      var colors = []
+      var randomIndex = Math.floor(Math.random() * 3); // returns random index to make brighter color
+      for (let i = 0; i < 3; i++) {
+        if (i === randomIndex) {
+          colors.push(randomInt(70, 255)) 
+        } else {
+          colors.push(randomInt(0, 255))
+        } 
+      }
+      return `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`
+  }
+  
+  function randomInt(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+function drawColors(selectedColor, tile){
+    if (selectedColor == null) return
+    if (tile.classList.contains("f1")){
+        tile.setAttribute('fill', selectedColor.face1)
+        tile.setAttribute('stroke', selectedColor.face1)
+      }
+      else if (tile.classList.contains("f3")){
+        tile.setAttribute('fill', selectedColor.face3)
+        tile.setAttribute('stroke', selectedColor.face3)
+      }
+      else {
+        tile.setAttribute('fill', selectedColor.face2)
+        tile.setAttribute('stroke', selectedColor.face2)
+      }
+}
+
+function initializeColor(colorOjbect, faceTwoRBG){
+    colorOjbect.face2 = faceTwoRBG;
+    colorOjbect.face3 = shiftColor(faceTwoRBG, "shiftDown")
+    colorOjbect.face1 = shiftColor(faceTwoRBG, "shiftUp" )
+}
+
+
+initializeColor(color1, randomRGB())
+initializeColor(color2,"rgb(172,172,172)")
+
+
+function updateColors(rgbValue){
+    darkColor = shiftColor(mediumColor, "shiftDown")
+    lightColor = shiftColor(mediumColor, "shiftUp" )
+}
+
+
+
+
 // var changeColorBtn1 = document.getElementById("changeColorOne")
 // var changeColorBtn2 = document.getElementById("changeColorTwo")
 
 
 // changeColorBtn1.addEventListener("click", function(){
-//   var colorOneSVGArray = document.querySelectorAll('.color1')
+//   var colorOneSVGArray = document.querySelectorAll('.color')
 //   changeColors(colorOneSVGArray)
 // });
 
@@ -12,135 +138,38 @@
 //   changeColors(colorTwoSVGArray)
 // });
 
-// function changeColors(svgArray) {
-//   var mediumColor = random_rgb()
-//   var darkColor = shiftColor(mediumColor, "shiftDown")
-//   var lightColor = shiftColor(mediumColor, "shiftUp" )
-
-//   updateBrowserColor(mediumColor)
-
-//   for (let i = 0; i < svgArray.length; i++) {
-//     var color1 = svgArray[i]
-//     if (color1.classList.contains("face1")){
-//       color1.setAttribute('fill', lightColor)
-//     }
-//     else if (color1.classList.contains("face3")){
-//       color1.setAttribute('fill', darkColor)
-//     }
-//     else {
-//       color1.setAttribute('fill', mediumColor)
-//     }
-//   }
-// }
-
-// function random_rgb() {
-//     var colors = []
-//     var randomIndex = Math.floor(Math.random() * 3); // returns random index to make brighter color
-//     for (let i = 0; i < 3; i++) {
-//       if (i === randomIndex) {
-//         colors.push(randomInt(70, 255)) 
-//       } else {
-//         colors.push(randomInt(0, 255))
-//       } 
-//     }
-//     return `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`
-// }
-
-// function randomInt(min, max) { // min and max included 
-//   return Math.floor(Math.random() * (max - min + 1) + min);
-// }
-
-// function updateBrowserColor(color){
-//   var browserMetaData = document.getElementById('metaData')
-//   browserMetaData.setAttribute("content", color)
-// }
-
-// function shiftColor(color ,upOrDown){
-//   var colorArray = color.substring(4).slice(0, -1).split(",")
-//   var newColor = []
-//   var range = 40
-//   var shiftDown;
-//   if (upOrDown !== "shiftUp") shiftDown = true;
-
-//   for (let i = 0; i < colorArray.length; i++) {
-//     var color = parseInt(colorArray[i])
-//     if (shiftDown){
-//       if (color - range < 0 ) {
-//         newColor.push(0)
-//       } else {
-//         newColor.push(color - range)
-//       }
-//     } else {
-//       if (color + range > 255 ) {
-//         newColor.push(255)
-//       } else {
-//         newColor.push(color + range)
-//       }
-//     }
-//   }
-//   return `rgb(${newColor[0]},${newColor[1]},${newColor[2]})`
-// }
 
 
-//   function dillanYouSonOfABitch(){
-//   console.log(`
-//                                                 @@@@@@@@@@@@@@@@@@@@@                                                                                  
-//                                               . ***/*,,*******/%##%(                                                                                  
-//                                               /#/***//*///////(##&%&                                                                                  
-//                                               % ////*****/***#@&%%%(                                                                                  
-//                                             ((,#%#(((#(/////(%#@@@@&                                                                                  
-//                                             .,/(#%%%#((#@@@@&@@@%#(&%/                                                                           *#/&@&
-//                                               */(#(((#%@@@@@@@@@&&&(%%%                                                                    *%%%&&(%&/&@@
-//   .    .... *                                 ((%&%&&&@&&@@@&@@&%&&@%&%#                                                         ,//(%%&&&&&&@@@&&&&*(&@
-//   ........,****..                            ,##(#%&@@@@@@&&%&&&%(&&&&%##,                                                   /*/(#&&&&&&&&&&&&&&&&&&&&&&
-//   ...,,.,.......**,,                         ,####%&&&&@@@&%&&@&#((%##%%##                                               .%(#(%%%%%&&%&&&&&&&&&&&&&&&&&&
-//   ,,,,.......,/*/**,.*                        *&#%##%%&@@@@@@@&@%&%###%&&%/                                        ,*,.**%%%%%%%%%%%%%%&&&&&&&&&&&&&&&&&
-//   ,,.......,**.     ..,.                        %%%###%#&@&@@@&&%((###&@@%(*                                     *.,(#%%&%%%%%%%%%%%%%%%%%%&&&&&&&&&&&&&
-//   ........**,.      ..**..                       *%%####%#%%@&&&&%###&&&&@@#                                   .*,*%/%%%%%%#%%%%%%%%%%%%%%%&&&&&&%&&&&&&
-//   ,.,..,,,/,    ...,**/**,.                        /%&%#%#%#&%%&%%#%%&@%&%@                                 //&&&%%%%&%%%%%&%%&%%%%%%%%%%%%&&&&&&&&&&&&&
-//   ,.,,,,,*,,..,...,,*,..                            #%###(#%%%%%((##&@@@@@&                              ,,(%%%#&&%%&%%&&&&&%&&%%%%%%%%%%%%&&&&&&&&&&&&&
-//   ,,,,,****,*,.. .,,....   ,.                       ,(##%#(%#&%((##%@&&&&@.                         ,///**/##%%%%&#&%%%%&&&&&&&%%%%%%%%%%%%&&&&&&&&&&&&&
-//   ,,****,,,,........ ...   . ..                       ##( (%%%###%#&&@&%&&.                     //*///////****#%%%%&%&%%%%&&&&&%%&&&&%%%%&%&&&&&&&&&&&&&
-//   ////**,,,.,...,,,**,,   .  .*,                     . # (#%#(((#%&%&&@@&###(                /////*/**,****,,***%%%%%&%&&&&&&@&&%&&&&&&%&&&&&&&&&&&&&&&&
-//   ###//**,,.,,,*//**,.      ,.    .                   , /(########%&%%&&@#((##/.            ((////***,,,,,.,,,,***&%%&&&&&&&&@@&&&&&&&&&&&&&&&&&&&&&&&&&
-//   %%#(//***,,*///,,.      /*.,.      .               ,  %((((((((#@&#%&@@*#((####(.        ,((/////*,, .****//(///(&&&&&&&@@@@@&&&&&&&&&&&&&&&&&&&&&&&&&
-//   %%##(//,**//(**.     */*./,.          ,         ., ##%#######(##%@&&&@@//#((((##(#      (###(////*,**////((#((#%#%&&&&&@&@@@@@&&&&&&&@&&&&&&&&&&&&&&&&
-//   @%%(**//*/((,.     //*/*.             /%*     .**%%&%%%%%#%%%%%#%%&&&@%((((((/(((%%%(*(#(((((((//////(((#####%&&@@@@&&&&@@@@@@&&&&&@&@@@&&&&&&&&&&&&&&
-//   &#/#(((*((/,..   ,/(/,  .             (&&%,   .  (&%%%%%###%%%%%&%&&@@&%#(((((///###/##/((///(#%##%%%%%%%&&&@@@@&#(&@&&@@@@@@@@&&@&@@@@@&&&&&&&&@&&&&&
-//   @@@&%//#(/*.,..,/#/,...             . %&@@&&/  (@@%%######%%#%%%&&&@@@&%##(((/(((##((((((/(//**(#%#%#%%&@@@@&#/(##(#@@@@@@@@@@@&&@&@@@@@@&@&&&&&&&&@@@
-//   @@%##((#/**,.,/%(/*,.               *%&@@@&@@@@@&#%##########%&@&@@@@@&&%%###((((((((((/**(#(%#((////((#((##(##(#(#%&@@@@@@@@@@@@@@@@@@@@@&&&&&&@&@@@@
-//   @@&@&###/*,,/@%%/**,.           . ,(%&&@@&&&&&@&%##%###%%%%&&@&@@&@@@&&&&&%##((((##((((@@%%#((/*,,,*/(##%%#(///(((%&@@@@@@@@@@@@@@@@@@@@@@@&&&&@&@@@@@
-//   @@@@&%%%#%@@@%%#/*,,,,.,,.....  /%%&&@@@@@@@%%%%#%%(###(%&@@&@@@@@@@&@@&&&%%%##%###(#&//(((#((/**/#####(%##(((#%&&@@@@@@@#...&@@@@@@@@@@@@@@@@@@@@@@@@
-//   @@@@&&&@@@@@&%(((**,/**,,,,,,.@@@@@@@@@@@@@@&&&@&%##(#%&@@@@@@@@@@@@@@@@@@&&&%%%%#(((%#((((((/,,**/(#%&&#%###%&@@@@@@@@&,     (@@@@@@@@@@@@@@@&@@@@@@@
-//   @@@@@@@@@@@&%%#####///****,%@@@@@@@@@@@@@@@@@@@@&%%###%@@@@@@@@@@@@@@@@@&@@@&&&%%%%%@&%###/*****((%&@%%&%#%@@@@@@@@@%.         #@@@@@@@@@@@@@@@@@@@@@@
-//   @@@@@@@@@@@@&%&%%###(((*@@@@@@@@@@@@@@@@@@@@@@@@@&&&%%@@@@@@@@@@@@@@@@@@@@@@@@@@&&%@&&%##(***//(%&@%@@@@%@@@@@@@@/             .&@@@@@@@@@@@@@@@@@@@@@
-//   @@@@@@@@@@@@@@@%%##(@@@@@@@@@@@@@@@@@@@@@@@@@@@&&@&&@@@@@@@@@@@@@# @@@@@@@@@@@@@@@@@@@%#((((%%&@@@@@@@@@@@@@%*                  /@@@@@@@@@@@@@@@@@@@@@
-//   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@&    @@@@@@@@@@@@&@&&%#%%&@@@@@@@@@@@@@%*                       .%@@@@@@@@@@@@@@@@@@@@
-//   @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@      #@@@@@@@@@@@@%#%%@@@@@@@@@@@%.                            ./@@@@@@@@@@@@@@@@@@@@
-//   `)
-//   console.log("%cLets team up!","font-size: 20px");
-//   console.log("%chttps://www.linkedin.com/in/samseabourn 💬","color: blue; font-size: 20px");
-//   }
+function shiftColor(color ,upOrDown){
+  var colorArray = color.substring(4).slice(0, -1).split(",")
+  var newColor = []
+  var range = 80
+  var shiftDown;
+  if (upOrDown !== "shiftUp") shiftDown = true;
 
-let width
-let height
-const changeColorMenu = document.getElementById('changeColorMenu')
-
-window.addEventListener("resize", resizeSVG);
-
-
-
-function resizeSVG(){
-    width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    height = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight;
-    console.log(width, height);
-    changeColorMenu
-    // test.setAttribute("width", width)
-    // test.setAttribute("height", height)
-    // test.setAttribute("viewBox", `0 0 ${width} ${height}`)
-
+  for (let i = 0; i < colorArray.length; i++) {
+    var color = parseInt(colorArray[i])
+    if (shiftDown){
+      if (color - range < 0 ) {
+        newColor.push(0)
+      } else {
+        newColor.push(color - range)
+      }
+    } else {
+      if (color + range > 255 ) {
+        newColor.push(255)
+      } else {
+        newColor.push(color + range)
+      }
+    }
+  }
+  return `rgb(${newColor[0]},${newColor[1]},${newColor[2]})`
 }
-resizeSVG()
+
+// changeColors(allTiles, "rgb(172,172,172)") //Setting to grey
+
+
+
 
 
 
